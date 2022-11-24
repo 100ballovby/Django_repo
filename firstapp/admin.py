6e5f5716ea0784ea2701378aaf9c8ae57c100e90
demidraksin <greatraksin@icloud.com>
2022-11-24 17:03:20 +0300

    Adding first model

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Product
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    # данные, которые отображаются в списке сущностей в админке
+    list_display = ('article', 'name', 'price', 'in_stock',)
+    # по каким данным буду фильтровать элементы
+    list_filter = ('in_stock',)
+    # некоторые поля элементов можно менять прямо из общего списка
+    list_editable = ('price', 'in_stock',)
+    # критерий поиска
+    search_fields = ('article', 'name',)
+
+admin.site.register(Product, ProductAdmin)  # отображаю в админке новую модель
--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    article = models.IntegerField('Артикул', max_length=30,
+                                  db_index=True)
+    name = models.CharField('Название товара', max_length=100,
+                            db_index=True)
+    image = models.URLField('Ссылка на изображение товара',
+                            default='https://cdn-icons-png.flaticon.com/512/1376/1376786.png')
+    description = models.TextField('Описание товара')
+    price = models.FloatField('Стоимость')
+    in_stock = models.BooleanField('Товар в наличии', default=True)
+
+    def __str__(self):  # отвечает за внешний вид записи в БД
+        return f'{self.name} {self.article} | {self.price}'
+
+    class Meta:  # настройка отображения вашей модели в административной панели Django
+        ordering = ['article', 'name',]  # фильтрация объектов в административной панели
+        verbose_name = 'Товар'  # название объекта в административной панели
+        verbose_name_plural = 'Товары'  # название раздела с сущностями в админке
+
+
+
+
