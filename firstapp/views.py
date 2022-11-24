9d2865b4aad061575c7ec2e3d67878ce61c10edc
demidraksin <greatraksin@icloud.com>
2022-11-24 17:23:43 +0300

    Creating view for queryset from db

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from .forms import UserForm
+from .models import Product
 # Create your views here.
 
 
 def index(request):
+    products = Product.objects.all()  # достать все продукты
+    print(products)
+    return render(request, 'firstapp/index.html', {
+        'title': 'Главная страница',
+        'prod': products,
+    })
+
+
+def about(request):
+    return HttpResponse('<h1>About page</h1>')
+
+
+def contacts(request):
     """
-    Функция будет генерировать главную страницу ПРИЛОЖЕНИЯ
-    firstapp.
-    :param request: запрос на получение страницы от пользователя
-    :return: текст из функци HttpResponse
-    """
+        Функция будет генерировать главную страницу ПРИЛОЖЕНИЯ
+        firstapp.
+        :param request: запрос на получение страницы от пользователя
+        :return: текст из функци HttpResponse
+        """
     if request.method == 'POST':
         name = request.POST.get('name')  # получаю инфу по имени поля
         age = request.POST.get('age')
@@ -22,11 +36,3 @@ def index(request):
                    'form': userform}
         return render(request, 'firstapp/index.html',
                       context=context)
-
-
-def about(request):
-    return HttpResponse('<h1>About page</h1>')
-
-
-def contacts(request):
-    return HttpResponse('<h1>Contacts</h1>')
