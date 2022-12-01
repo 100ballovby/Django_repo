b3c456d96884bb03edfc027148762e89a53a48c5
demidraksin <greatraksin@icloud.com>
2022-12-01 16:35:29 +0300

    Adding new app - user

--- Django_repo/settings.py
+++ Django_repo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'firstapp',  # название вашего нового приложения для сайта
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'  # конфигурационная переменная для форм
