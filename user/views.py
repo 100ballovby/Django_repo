4710748d2005d0b1dbfe3f265e1f65edad019957
demidraksin <greatraksin@icloud.com>
2022-12-08 16:33:36 +0300

    Adding header template

--- user/views.py
+++ user/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
 
 # Create your views here.
 def user_home(request):
     return render(request, 'user/index.html')
+
+
+class SignUp(CreateView):
+    forms_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
